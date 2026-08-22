8a84c2dc0391e7c8c2dc753f7581274cc9fdd61a
King Thomas <king_thomas@example.local>
2026-08-22 09:22:01 +0100

    Add TPL server config

---
+++ config/custom-formats.ts
@@ -0,0 +1,16 @@
+export const Formats = [
+	{
+		section: "Draft League",
+		column: 1,
+	},
+
+	{
+		name: "[Gen 9] 2026 TPL Draft League",
+		mod: 'gen9',
+		gameType: 'multi',
+		ruleset: [
+			'Standard AG',
+			'NatDex Mod',
+		],
+	},
+];
